1ab0751ac332ecd20bfc72acd1d0b3464a61b97e
Vishal <vishalmishrasep491@gmail.com>
2024-07-17 16:43:44 +0530

    Day-3

---
+++ 48. Rotate Image.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        // First to transpose and then reverse the rows
+        int n=matrix.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
